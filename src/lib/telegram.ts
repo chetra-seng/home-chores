972ff265c540a4627a51227f1defc027b32047df
chetra-seng <sengchetra.love@icloud.com>
2023-12-31 04:53:18 +0700

    Fixed static chat id

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -3,6 +3,6 @@ const TELEGRAM_URL = 'https://api.telegram.org/bot';
 
 export const sendMessage = async (message: string, option: string) => {
 	await fetch(
-		`${TELEGRAM_URL}${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002139472082&text=${message}&reply_markup=${option}`
+		`${TELEGRAM_URL}${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${env.TELEGRAM_CHAT_ID}&text=${message}&reply_markup=${option}`
 	);
 };
